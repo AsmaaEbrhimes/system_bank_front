fbea41f2eee8dc15dc4a2d9f30fd9e6e403a222c
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-07 04:30:37 +0300

    feat: Add AuthBanner component with HTML and SCSS styles

---
+++ src/app/Modules/auth/auth-banner/auth-banner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-banner',
+  standalone: false,
+  templateUrl: './auth-banner.html',
+  styleUrl: './auth-banner.scss',
+})
+export class AuthBanner {}
