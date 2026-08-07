738ee31f01e46fc5a6268fd75c6765086cf68fc2
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-07 04:30:11 +0300

    feat: Add AuthBanner component to AuthModule declarations

--- src/app/Modules/auth/auth-module.ts
+++ src/app/Modules/auth/auth-module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing-module';
 import { Login } from './login/login';
 import { Register } from './register/register';
+import { AuthBanner } from './auth-banner/auth-banner';
 
 @NgModule({
-  declarations: [Login, Register],
+  declarations: [Login, Register, AuthBanner],
   imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
