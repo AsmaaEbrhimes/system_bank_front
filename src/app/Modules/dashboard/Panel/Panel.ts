d610b62ffd0711916b731fac70ad0a53d79539e6
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-08 17:38:10 +0300

    feat: Create Panel component with HTML, SCSS, and TypeScript files

---
+++ src/app/Modules/dashboard/Panel/Panel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  standalone: false,
+  templateUrl: './Panel.html',
+  styleUrl: './Panel.scss',
+})
+export class Panel {}
