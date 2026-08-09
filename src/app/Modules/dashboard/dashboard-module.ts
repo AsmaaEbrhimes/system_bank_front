d863669b0f59575d57149c8286066f74ea5d0e9a
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-09 11:38:57 +0300

    feat: Update Dashboard module and routing to include MainPage and related components

--- src/app/Modules/dashboard/dashboard-module.ts
+++ src/app/Modules/dashboard/dashboard-module.ts
@@ -4,9 +4,14 @@ import { SvgIconComponent } from 'angular-svg-icon';
 import { DashboardRoutingModule } from './dashboard-routing-module';
 import { SideNav } from './side-nav/side-nav';
 import { Panel } from './Panel/Panel';
+import { MainPage } from './main-page/main-page';
+import { SummaryCard } from './main-page/summary-card/summary-card';
+import { DatePickerModule } from 'primeng/datepicker';
+import { SparklineChart } from './main-page/summary-card/sparkline-chart/sparkline-chart';
+import { TabelTransations } from './main-page/tabel-transations/tabel-transations';
 
 @NgModule({
-  declarations: [SideNav, Panel],
-  imports: [CommonModule, DashboardRoutingModule ,  SvgIconComponent ],
+  declarations: [SideNav, Panel, MainPage, SummaryCard, SparklineChart, TabelTransations],
+  imports: [CommonModule, DashboardRoutingModule, SvgIconComponent, DatePickerModule],
 })
 export class DashboardModule {}
--- src/app/Modules/dashboard/dashboard-routing-module.ts
+++ src/app/Modules/dashboard/dashboard-routing-module.ts
@@ -1,10 +1,19 @@
+import { MainPage } from './main-page/main-page';
 import { Panel } from './Panel/Panel';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 {}
 
 const routes: Routes = [
-  {path:"",component:Panel}
+  {
+    path:"",component:Panel,
+    children:[
+      {path:"",component:MainPage}
+    ]
+
+
+
+  }
 ];
 
 @NgModule({
