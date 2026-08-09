3d1c425000e452055814967c26875023a60d8e7a
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-09 11:40:00 +0300

    feat: Add TabelTransations component with HTML, SCSS, and TypeScript files

---
+++ src/app/Modules/dashboard/main-page/tabel-transations/tabel-transations.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabel-transations',
+  standalone: false,
+  templateUrl: './tabel-transations.html',
+  styleUrl: './tabel-transations.scss',
+})
+export class TabelTransations {}
