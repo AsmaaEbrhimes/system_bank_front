6b1063c9cfeb00c33b318b2a183064ddcf435b4f
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-09 16:41:04 +0300

    feat: Update SharedModule to include Tabel component and DatePipe

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, DatePipe } from '@angular/common';
 import { Success } from './success/success';
 import { NoData } from './no-data/no-data';
 import { Error } from './error/error';
 import { Loader } from './loader/loader';
+import { TableModule } from 'primeng/table';
+import { Tabel } from './tabel/tabel';
 
 @NgModule({
-  declarations: [Success , NoData , Error , Loader ],
-  imports: [CommonModule , AsyncPipe ],
-  exports: [Success , NoData , Error , Loader ]
+  declarations: [Success , NoData , Error , Loader , Tabel],
+  imports: [CommonModule , AsyncPipe , TableModule ],
+  exports: [Success , NoData , Error , Loader , Tabel ],
+  providers: [
+    DatePipe 
+  ]
 })
 export class SharedModule {}
