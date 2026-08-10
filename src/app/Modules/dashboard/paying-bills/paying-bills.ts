b72fa8576b049af800607dd82be250419fb96f46
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-10 16:44:34 +0300

    feat: Add PayingBills component with HTML and SCSS files

---
+++ src/app/Modules/dashboard/paying-bills/paying-bills.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paying-bills',
+  standalone: false,
+  templateUrl: './paying-bills.html',
+  styleUrl: './paying-bills.scss',
+})
+export class PayingBills {}
