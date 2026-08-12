b876faa8f7ba5dd68e8cbf4dc84fa70d0e420b69
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-12 18:10:22 +0300

    feat: Add BillingMetrics component with SCSS styling for dashboard

---
+++ src/app/Modules/dashboard/paying-bills/billing-metrics/billing-metrics.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-metrics',
+  standalone: false,
+  templateUrl: './billing-metrics.html',
+  styleUrl: './billing-metrics.scss',
+})
+export class BillingMetrics {}
