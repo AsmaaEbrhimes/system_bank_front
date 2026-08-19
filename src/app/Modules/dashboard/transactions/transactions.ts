de4c49cf6735e58a92c1b532c51a2c7192d3d342
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-19 10:26:16 +0300

    feat: Add Transactions component with HTML, SCSS, and TypeScript files

---
+++ src/app/Modules/dashboard/transactions/transactions.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions',
+  standalone: false,
+  templateUrl: './transactions.html',
+  styleUrl: './transactions.scss',
+})
+export class Transactions {}
