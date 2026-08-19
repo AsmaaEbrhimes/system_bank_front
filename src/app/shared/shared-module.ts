a0596b08fca427ef1ac623b143d22c9b3cd3233c
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-19 16:13:05 +0300

    feat: Update SharedModule to include LoaderComponent in declarations and exports

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -3,16 +3,15 @@ import { AsyncPipe, CommonModule, DatePipe } from '@angular/common';
 import { Success } from './success/success';
 import { NoData } from './no-data/no-data';
 import { Error } from './error/error';
-import { Loader } from './loader/loader';
 import { TableModule } from 'primeng/table';
 import { Tabel } from './tabel/tabel';
 import { SearchComponent } from './search/search.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-
+import { LoaderComponent } from './loader/loader.component';
 @NgModule({
-  declarations: [Success , NoData , Error , Loader , Tabel , SearchComponent],
+  declarations: [Success , NoData , Error  , Tabel , SearchComponent , LoaderComponent],
   imports: [CommonModule , AsyncPipe , TableModule , AngularSvgIconModule],
-  exports: [Success , NoData , Error , Loader , Tabel , SearchComponent],
+  exports: [Success , NoData , Error  , Tabel , SearchComponent, LoaderComponent],
   providers: [
     DatePipe
   ]
