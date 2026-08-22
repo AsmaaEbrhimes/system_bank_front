14959c51b4163e10e2dc5281ddf498ab3999462b
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-22 22:42:10 +0300

    feat: Add ReactiveFormsModule to AuthModule imports for form handling

--- src/app/Modules/auth/auth-module.ts
+++ src/app/Modules/auth/auth-module.ts
@@ -5,8 +5,9 @@ import { Login } from './login/login';
 import { Register } from './register/register';
 import { AuthBanner } from './auth-banner/auth-banner';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [Login, Register, AuthBanner],
-  imports: [CommonModule, AuthRoutingModule,SvgIconComponent],
+  imports: [CommonModule, AuthRoutingModule,SvgIconComponent , ReactiveFormsModule],
 })
 export class AuthModule {}
