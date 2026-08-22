4de76451d7fcd52cf1848d46c88a233bc8e26439
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-22 22:42:18 +0300

    feat: Implement reactive form handling in registration component with validation

--- src/app/Modules/auth/register/register.ts
+++ src/app/Modules/auth/register/register.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Data } from '../../../core/Servies/data';
 
 @Component({
   selector: 'app-register',
@@ -6,4 +8,40 @@ import { Component } from '@angular/core';
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+export class Register implements OnInit {
+  //=========================Implementions=============================//
+  ngOnInit(): void {
+    this.createForm();
+  }
+  constructor(
+    private FB: FormBuilder,
+    private Data: Data,
+  ) {}
+
+  //=========================Varibels=============================//
+  Form = signal<FormGroup>(new FormGroup({}));
+
+  //=========================Functions=============================//
+  createForm() {
+    this.Form.set(
+      this.FB.group({
+        name: ['', Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required],
+        confirmePassword: ['', Validators.required],
+      }),
+    );
+  }
+
+  onSubmit() {
+    if (this.Form().invalid) {
+      this.Form().markAllAsTouched();
+      return;
+    }
+    this.Data.post('Auth/Register', this.Form().value).subscribe((res) => {});
+  }
+
+  getControlName(controlName: string) {
+    return this.Form().get(controlName);
+  }
+}
