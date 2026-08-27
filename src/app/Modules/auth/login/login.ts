781798b42e254dfa12f6ecf61c1c40de4a0be3ea
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-27 23:17:16 +0300

    feat: Implement reactive form handling in login component with validation and error messages

--- src/app/Modules/auth/login/login.ts
+++ src/app/Modules/auth/login/login.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Data } from '../../../core/Servies/data';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +10,43 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login implements OnInit {
+  constructor(
+    private FB: FormBuilder,
+    private Data: Data,
+    private Router: Router,
+  ) {}
+  ngOnInit(): void {
+    this.CreateForm();
+  }
+
+  //=========================Varibels=============================//
+  Form = signal<FormGroup>(new FormGroup({}));
+
+  CreateForm() {
+    this.Form.set(
+      this.FB.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required],
+      }),
+    );
+  }
+
+  OnSubmit() {
+    if (this.Form().invalid) {
+      this.Form().markAllAsTouched();
+    }
+    this.Data.post('Auth/Login', this.Form().value).subscribe((res) => {
+      this.HandelResponseSuccess(res);
+    });
+  }
+
+  HandelResponseSuccess(res: any) {
+    this.Router.navigate(['/ContentDashboard']);
+    sessionStorage.setItem("token",res.token)
+  }
+
+  getControlName(controlName: string) {
+    return this.Form().get(controlName);
+  }
+}
