7f8b52b31308e87af4b7fce7d43c94c1233fb5b2
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-08-29 12:47:12 +0300

    feat: Refactor TabelTransations component to use reactive data handling and clean up unused code

--- src/app/Modules/dashboard/main-page/tabel-transations/tabel-transations.ts
+++ src/app/Modules/dashboard/main-page/tabel-transations/tabel-transations.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-tabel-transations',
@@ -6,51 +6,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tabel-transations.html',
   styleUrl: './tabel-transations.scss',
 })
-export class TabelTransations implements OnInit {
-  bodyTableCols: any[] = [];
+export class TabelTransations {
+  data = signal<any[]>([]);
 
-  reportData = [
-    {
-      initials: "م.ك",
-      customer: "محمود كامل",
-      type: "تحويل صادر",
-      amount: "-1,250.00 ج.م",
-      status: "تم التنفيذ",
-      time: "10:30 صباحاً"
-    },
-    {
-      initials: "أ.ع",
-      customer: "أحمد علي",
-      type: "إيداع وارد",
-      amount: "+5,000.00 ج.م",
-      status: "تم التنفيذ",
-      time: "11:15 صباحاً"
-    },
-    {
-      initials: "س.م",
-      customer: "سارة محمد",
-      type: "دفع فاتورة",
-      amount: "-450.00 ج.م",
-      status: "قيد المعالجة",
-      time: "01:45 مساءً"
-    },
-    {
-      initials: "ع.ح",
-      customer: "عمر حسن",
-      type: "تحويل وارد",
-      amount: "+2,100.00 ج.م",
-      status: "تم التنفيذ",
-      time: "03:20 مساءً"
-    }
-  ];
+  @Input()
+  set Transactions(value: any) {
+    console.log(value);
+    this.data.set(value);
+    this.boadyTabel();
+  }
 
-  ngOnInit(): void {
-    this.bodyTableCols = [
-      { key: "العميل", value: "customer" },
-      { key: "النوع", value: "type" },
-      { key: "المبلغ", value: "amount" },
-      { key: "الحالة", value: "status" },
-      { key: "الوقت", value: "time" }
+  boadyTabel() {
+    return [
+      { key: 'Amount', value: 'amount' },
+      { key: 'pament Amount', value: 'balanceAfter' },
+      { key: 'CreatedAt', value: 'createdAt', type: 'date', format: 'dd/MM/yyyy' },
     ];
   }
 }
