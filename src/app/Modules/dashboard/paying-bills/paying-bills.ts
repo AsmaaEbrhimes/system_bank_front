af6a5295bb557c23b35e1b0066219210a330f700
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-01 18:03:00 +0300

    refactor: Simplify app-tabel component usage and clean up unused code in paying-bills module

--- src/app/Modules/dashboard/paying-bills/paying-bills.ts
+++ src/app/Modules/dashboard/paying-bills/paying-bills.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Data } from '../../../core/Servies/data';
 
 @Component({
   selector: 'app-paying-bills',
@@ -6,51 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './paying-bills.html',
   styleUrl: './paying-bills.scss',
 })
-export class PayingBills {
-  bodyTableCols: any[] = [];
-  activeTab: 'unpaid' | 'paid' = 'unpaid';
-  reportData = [
-    {
-      initials: 'م.ك',
-      customer: 'محمود كامل',
-      type: 'تحويل صادر',
-      amount: '-1,250.00 ج.م',
-      status: 'تم التنفيذ',
-      time: '10:30 صباحاً',
-    },
-    {
-      initials: 'أ.ع',
-      customer: 'أحمد علي',
-      type: 'إيداع وارد',
-      amount: '+5,000.00 ج.م',
-      status: 'تم التنفيذ',
-      time: '11:15 صباحاً',
-    },
-    {
-      initials: 'س.م',
-      customer: 'سارة محمد',
-      type: 'دفع فاتورة',
-      amount: '-450.00 ج.م',
-      status: 'قيد المعالجة',
-      time: '01:45 مساءً',
-    },
-    {
-      initials: 'ع.ح',
-      customer: 'عمر حسن',
-      type: 'تحويل وارد',
-      amount: '+2,100.00 ج.م',
-      status: 'تم التنفيذ',
-      time: '03:20 مساءً',
-    },
-  ];
+export class PayingBills  {
 
-  ngOnInit(): void {
-    this.bodyTableCols = [
-      { key: 'العميل', value: 'customer' },
-      { key: 'النوع', value: 'type' },
-      { key: 'المبلغ', value: 'amount' },
-      { key: 'الحالة', value: 'status' },
-      { key: 'الوقت', value: 'time' },
+  activeTab: 'unpaid' | 'paid' = 'unpaid';
+
+
+  boadyTabel() {
+    return [
+      { key: 'Amount', value: 'amount' },
+      { key: 'balance After', value: 'balanceAfter' },
+      { key: 'CreatedAt', value: 'createdAt', type: 'date', format: 'dd/MM/yyyy' },
     ];
   }
 }
