2d6ec0586ff5bbfa907cde4d3ff6b28af742c63a
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-01 15:46:58 +0300

    feat: Implement reactive data handling in summary-card component

--- src/app/Modules/dashboard/main-page/summary-card/summary-card.ts
+++ src/app/Modules/dashboard/main-page/summary-card/summary-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-summary-card',
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './summary-card.html',
   styleUrl: './summary-card.scss',
 })
-export class SummaryCard {}
+export class SummaryCard {
+  transactions = signal<any>('');
+  cards = signal<any>('');
+  custmers = signal<any>('');
+  loans = signal<any>('');
+
+  @Input()
+  set CardsData(value: any) {
+    this.cards.set(value);
+  }
+
+  @Input()
+  set CustmersData(value: any) {
+    this.custmers.set(value);
+  }
+
+  @Input()
+  set TransactionsData(value: any) {
+    this.transactions.set(value);
+  }
+
+  @Input()
+  set loansData(value: any) {
+    this.loans.set(value);
+  }
+}
