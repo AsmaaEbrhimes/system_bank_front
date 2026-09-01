8aac4bd3d3bff917ff8b1851d8327876c308e5bb
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-01 15:46:52 +0300

    feat: Bind data to app-summary-card component in main page

--- src/app/Modules/dashboard/main-page/main-page.ts
+++ src/app/Modules/dashboard/main-page/main-page.ts
@@ -12,12 +12,37 @@ export class MainPage implements OnInit {
 
   ngOnInit(): void {
     this.getAllTransactions();
+    this.getAllcards();
+    this.getAllCustmers();
+    this.getAllLoans();
   }
 
+  transactions = signal<any>('');
+  cards = signal<any>('');
+  custmers = signal<any>('');
+  loans = signal<any>('');
 
-  transactions=signal<any>("")
+  getAllcards() {
+    this.Data.get('Cards').subscribe((res) => {
+      this.cards.set(res);
+    });
+  }
+
+  getAllCustmers() {
+    this.Data.get('Customers').subscribe((res) => {
+      this.custmers.set(res);
+    });
+  }
 
   getAllTransactions() {
-    this.Data.get('Transactions').subscribe((res) => {this.transactions.set(res)});
+    this.Data.get('Transactions').subscribe((res) => {
+      this.transactions.set(res);
+    });
+  }
+
+  getAllLoans() {
+    this.Data.get('Loans/pending-requests').subscribe((res) => {
+      this.loans.set(res);
+    });
   }
 }
