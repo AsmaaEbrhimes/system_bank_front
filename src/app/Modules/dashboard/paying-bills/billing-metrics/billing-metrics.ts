3a8dfa9099751589ab1c14218fa269d6ba201203
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-02 14:59:59 +0300

    feat: Implement customer selection and data fetching in billing-metrics component

--- src/app/Modules/dashboard/paying-bills/billing-metrics/billing-metrics.ts
+++ src/app/Modules/dashboard/paying-bills/billing-metrics/billing-metrics.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { Data } from '../../../../core/Servies/data';
 
 @Component({
   selector: 'app-billing-metrics',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './billing-metrics.html',
   styleUrl: './billing-metrics.scss',
 })
-export class BillingMetrics {}
+export class BillingMetrics {
+  constructor(private Data: Data) {}
+
+  customers = signal<any[]>([]);
+  @Output() selectedCustomerId = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.getAllCustmers();
+  }
+
+  getAllCustmers() {
+    this.Data.get(`Customers`).subscribe((res: any) => {
+      this.customers.set(res);
+    });
+  }
+
+  onSelectCustomer(event: { value: any }) {
+    const selectedCustomerId = event.value;
+    this.selectedCustomerId.emit(selectedCustomerId);
+  }
+}
