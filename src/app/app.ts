39c211421fa026f9c239ceaab44d0b57b361d53e
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-06 12:34:29 +0300

    feat: Add SignalRService import and constructor to App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { SignalRService } from './shared/signal-r';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('SystemBank');
+  constructor(private signalRService: SignalRService) {}
 }
