ce2fbb6434b34e2091ea775d0ebf0f73fba37d32
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-06 12:35:19 +0300

    feat: Add Notifications component and SignalR service to SharedModule

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -8,12 +8,12 @@ import { Tabel } from './tabel/tabel';
 import { SearchComponent } from './search/search.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { LoaderComponent } from './loader/loader.component';
+import { Notifications } from './notifications/notifications';
+import { SignalRService } from './signal-r';
 @NgModule({
-  declarations: [Success , NoData , Error  , Tabel , SearchComponent , LoaderComponent],
-  imports: [CommonModule , AsyncPipe , TableModule , AngularSvgIconModule],
-  exports: [Success , NoData , Error  , Tabel , SearchComponent, LoaderComponent],
-  providers: [
-    DatePipe
-  ]
+  declarations: [Success, NoData, Error, Tabel, SearchComponent, LoaderComponent, Notifications],
+  imports: [CommonModule, AsyncPipe, TableModule, AngularSvgIconModule],
+  exports: [Success, NoData, Error, Tabel, SearchComponent, LoaderComponent , Notifications],
+  providers: [DatePipe , SignalRService],
 })
 export class SharedModule {}
