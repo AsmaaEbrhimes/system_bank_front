e6f0347cb813d7c771cc5fec32c2577228a2ba99
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-10 23:02:16 +0300

    feat: Update billing metrics component to use dynamic data for total due, total paid, and highest invoice amount

--- src/app/Modules/dashboard/paying-bills/billing-metrics/billing-metrics.ts
+++ src/app/Modules/dashboard/paying-bills/billing-metrics/billing-metrics.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
 import { Data } from '../../../../core/Servies/data';
 import { panel } from '../../panel.service';
 
@@ -9,18 +9,29 @@ import { panel } from '../../panel.service';
   styleUrl: './billing-metrics.scss',
 })
 export class BillingMetrics {
+  //===============================Implemantion================================//
   constructor(
     private Data: Data,
     private Panel: panel,
   ) {}
 
-  customers = signal<any[]>([]);
-  @Output() selectedCustomerId = new EventEmitter<string>();
-
   ngOnInit(): void {
     this.getAllCustmers();
   }
 
+  //===============================Vribels================================//
+  customers = signal<any[]>([]);
+  all_invoice = signal<any[]>([]);
+
+  //===============================Input & Output================================//
+  @Output() selectedCustomerId = new EventEmitter<string>();
+
+  @Input()
+  set data(value: any) {
+    this.all_invoice.set(value);
+  }
+
+  //===============================Functions================================//
   getAllCustmers() {
     this.Panel.Customers$.subscribe((res) => {
       this.customers.set(res);
@@ -30,5 +41,27 @@ export class BillingMetrics {
   onSelectCustomer(event: { value: any }) {
     const selectedCustomerId = event.value;
     this.selectedCustomerId.emit(selectedCustomerId);
+  }
+
+  get TotalDue(): number {
+    return (
+      this.all_invoice()
+        ?.filter((ele) => ele.isPaid == false)
+        .reduce((acc, b) => acc + b.amount, 0) || 0
+    );
+  }
+
+  get TotalPaid(): number {
+    return (
+      this.all_invoice()
+        ?.filter((ele) => ele.isPaid == true)
+        .reduce((acc, b) => acc + b.amount, 0) || 0
+    );
+  }
+
+  get HighestInvoiceAmount(): number {
+    const invoices = this.all_invoice();
+    if (!invoices || invoices.length === 0) return 0;
+    return Math.max(...invoices.map((b) => b.amount));
   }
 }
