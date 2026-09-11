6b25c5178b52fa66739572fae50ad328fafc639b
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-11 12:57:44 +0300

    feat: Enhance create card form with validation and dynamic form controls

--- src/app/Modules/dashboard/cards/create-cards/create-cards.ts
+++ src/app/Modules/dashboard/cards/create-cards/create-cards.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { Data } from '../../../../core/Servies/data';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-cards',
@@ -12,15 +13,18 @@ export class CreateCards implements OnInit {
   //=========================Implemantion===============================//
   ngOnInit(): void {
     this.getAllAccounts();
+    this.CreateForm();
   }
 
-  constructor(private Data: Data ,     private ref: DynamicDialogRef,
-) {}
+  constructor(
+    private Data: Data,
+    private ref: DynamicDialogRef,
+    private FB: FormBuilder,
+  ) {}
 
   //=========================Varibels===============================//
   accounts = signal<any>([]);
-
-
+  Form = signal<FormGroup>(new FormGroup({}));
 
   //=========================Functions===============================//
   getAllAccounts() {
@@ -29,7 +33,31 @@ export class CreateCards implements OnInit {
     });
   }
 
-  onCloseDilog(){
+  CreateForm() {
+    this.Form.set(
+      this.FB.group({
+        accountId: ['', Validators.required],
+        pin: ['', Validators.required],
+        cardType: ['', Validators.required],
+      }),
+    );
+  }
+
+  OnSubmit() {
+    if (this.Form().invalid) {
+      this.Form().markAllAsTouched();
+      return;
+    }
+    this.Data.post('Cards/request', this.Form().value).subscribe((res) => {
+      this.onCloseDilog();
+    });
+  }
+
+  onCloseDilog() {
     this.ref.close();
+  }
+
+  getControlName(controlName: string) {
+    return this.Form().get(controlName);
   }
 }
