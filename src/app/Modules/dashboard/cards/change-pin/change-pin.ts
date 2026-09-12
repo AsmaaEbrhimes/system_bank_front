df61d76eed8f6bc3194120e0294bcff9e663c01a
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-12 12:41:40 +0300

    feat: Add change PIN component with HTML, SCSS, and TypeScript files

---
+++ src/app/Modules/dashboard/cards/change-pin/change-pin.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-pin',
+  standalone: false,
+  templateUrl: './change-pin.html',
+  styleUrl: './change-pin.scss',
+})
+export class ChangePin {}
