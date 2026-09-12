cb56df69694328669fdc76d93ad81ea92f6bc30b
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-12 16:16:19 +0300

    feat: Implement change PIN functionality with form validation and UI updates

--- src/app/Modules/dashboard/cards/change-pin/change-pin.ts
+++ src/app/Modules/dashboard/cards/change-pin/change-pin.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Data } from '../../../../core/Servies/data';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-pin',
@@ -6,4 +9,60 @@ import { Component } from '@angular/core';
   templateUrl: './change-pin.html',
   styleUrl: './change-pin.scss',
 })
-export class ChangePin {}
+export class ChangePin implements OnInit {
+  //=========================Implemantion===============================//
+  ngOnInit(): void {
+    this.GetIdCards();
+    this.CreateForm();
+  }
+  constructor(
+    private Data: Data,
+    private ref: DynamicDialogRef,
+    private FB: FormBuilder,
+    private config: DynamicDialogConfig,
+  ) {}
+
+  //=========================Varibels===============================//
+  Form = signal<FormGroup>(new FormGroup({}));
+  cardId = signal<number>(0);
+
+  //=========================Functions===============================//
+
+  GetIdCards() {
+    this.cardId.set(this.config.data?.id);
+  }
+
+  CreateForm() {
+    this.Form.set(
+      this.FB.group({
+        oldPin: [
+          '',
+          [Validators.required, Validators.pattern('^[0-9]{4}$'), Validators.minLength(4)],
+        ],
+        newPin: [
+          '',
+          [Validators.required, Validators.pattern('^[0-9]{4}$'), Validators.minLength(4)],
+        ],
+      }),
+    );
+  }
+
+  onSubmit() {
+    if (this.Form().invalid) {
+      this.Form().markAllAsTouched();
+      return;
+    }
+    this.Data.put(`Cards/${this.cardId()}/change-pin`, this.Form().value).subscribe((res) => {
+      this.onCloseDilog();
+    });
+  }
+
+  onCloseDilog() {
+    this.ref.close();
+    this.Form().reset();
+  }
+
+  getControlName(controlName: string) {
+    return this.Form().get(controlName);
+  }
+}
