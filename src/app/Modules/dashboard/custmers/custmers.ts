84fb7e3e6526c4ab6aab7ac03a8b2a40f4fede82
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-23 14:43:11 +0300

    feat: Enhance customer table with account badge display and update data handling

--- src/app/Modules/dashboard/custmers/custmers.ts
+++ src/app/Modules/dashboard/custmers/custmers.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Data } from '../../../core/Servies/data';
 
 @Component({
   selector: 'app-custmers',
@@ -7,54 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './custmers.scss',
 })
 export class Custmers {
-ngOnInit(): void {
-this.bodyTabel()
-}
+  //=========================Implemantion===============================//
 
-  reportData = [
-  {
-    initials: "م.ك",
-    customer: "محمود كامل",
-    nationalId: "29801*********",
-    accountsCount: 2,
-    totalBalance: "84,220.00",
-    status: "نشط"
-  },
-  {
-    initials: "س.ع",
-    customer: "سارة عادل",
-    nationalId: "29505*********",
-    accountsCount: 1,
-    totalBalance: "12,940.50",
-    status: "نشط"
-  },
-  {
-    initials: "ي.ن",
-    customer: "يوسف ناصر",
-    nationalId: "30011*********",
-    accountsCount: 3,
-    totalBalance: "201,005.00",
-    status: "نشط"
-  },
-  {
-    initials: "ر.ط",
-    customer: "رنا طارق",
-    nationalId: "29712*********",
-    accountsCount: 1,
-    totalBalance: "5,430.00",
-    status: "مراجعة KYC"
+  ngOnInit(): void {
+    this.getData();
   }
-];
 
+  constructor(private Data: Data) {}
+  //=========================Varibels===============================//
+  data = signal<any>([]);
 
+  //=========================Functions===============================//
 
-bodyTabel(){
-  return [
-     { key: "العميل", value: "customer" },
-    { key: "الرقم القومي", value: "nationalId" },
-    { key: "عدد الحسابات", value: "accountsCount" },
-    { key: "الرصيد الكلي", value: "totalBalance" },
-    { key: "الحالة", value: "status" }
-  ]
-}
+  getData() {
+    this.Data.get(`Customers`).subscribe((res: any) => {
+      this.data.set(res);
+      this.bodyTabel();
+    });
+  }
+
+  bodyTabel() {
+    return [
+      { key: 'Full Name', value: 'fullName' },
+      { key: 'National ID', value: 'nationalId' },
+      { key: 'Phone Number', value: 'phoneNumber' },
+      { key: 'Created At', value: 'createdAt', type: 'date', format: 'dd/MM/yyyy' },
+    ];
+  }
 }
