b35ebff0e779a732645f1b28b11e181b8013e138
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-09-24 13:45:14 +0300

    feat: Add AddCustmerAndCreateAccount component with HTML template and SCSS styling

---
+++ src/app/Modules/dashboard/custmers/add-custmer-and-create-account/add-custmer-and-create-account.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-custmer-and-create-account',
+  standalone: false,
+  templateUrl: './add-custmer-and-create-account.html',
+  styleUrl: './add-custmer-and-create-account.scss',
+})
+export class AddCustmerAndCreateAccount {}
